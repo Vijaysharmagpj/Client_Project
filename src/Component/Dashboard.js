f65b218cb816d051cbe912b3e9bbf57755e02123
Vijaysharmagpj <vijaysharmagpj65@gmail.com>
2025-01-14 17:51:10 +0530

    dashboard commit

---
+++ src/Component/Dashboard.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div
+      className="w-[1338px] h-[679px] absolute"
+      style={{ top: "157px", left: "51px" }}
+    >
+      <p className="font-[DM Sans] text-[60px] font-bold leading-[76px] tracking-[0.5px] text-left underline from-font decoration-skip-ink">
+        APPLY INSTANT LOAN HERE FOR UPTO - 5 LACS NOW.
+      </p>
+      <span className="font-[DM Sans] text-[36px] font-medium leading-[64px] tracking-[0.5px] text-left underline from-font decoration-skip-ink">
+        IF YOU ARE A
+      </span>{" "}
+      <span className="font-[DM Sans] text-[36px] font-semibold leading-[64px] tracking-[0.5px] text-left underline from-font decoration-skip-ink">
+        (STOCK TRADERS/BAD CIBIL PROFILE/UN-EMPLOYED/SHOP KEEPERS/SMALL BUSINESSMEN...ETC)
+      </span>
+      <div className="w-[592px] h-[116px]">
+      <p className="font-[DM Sans] text-[22px] font-normal leading-[28.64px] tracking-[0.5px] text-left text-[#424545] underline from-font decoration-skip-ink">
+        Our loan services offer a hassle-free and streamlined borrowing experience, providing you with the funds you need in a timely manner to meet your financial requirements.
+      </p>
+    </div>
+    </div>
+  );
+};
+
+export default Dashboard;
