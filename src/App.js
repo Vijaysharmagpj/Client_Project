b77a990d21fa00aeaadfa18fc6d0520f904464c8
Prashant Singh <pprashantsingh2002@gmail.com>
2025-01-14 21:33:35 +0530

    prashant fist commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Component/Navbar";
 import Dashboard from "./Component/Dashboard";
+import FormandInfo from "./Component/FormandInfo";
 
 const App=()=>{
   return(
     <>
     <Navbar/>
     <Dashboard/>
+    <FormandInfo/>
     </>
   )
 }
