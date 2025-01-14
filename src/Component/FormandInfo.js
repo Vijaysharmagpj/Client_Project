bc7825d7f816e074029f5fa104308e0965eb3b3e
Prashant Singh <pprashantsingh2002@gmail.com>
2025-01-15 01:21:03 +0530

    new boxes

--- src/Component/FormandInfo.js
+++ src/Component/FormandInfo.js
@@ -1,11 +1,56 @@
 import React from "react";
-
+import "./FormandInfo.css";
+import images from "../assets";
 const FormandInfo = () => {
-    return (
-        <>
-          
-        </>
-    );
+    return(
+<>
+<div className="container">
+        <div className="header">
+            <p className="header-title">Achieve your Dreams With</p>
+            <p className="header-subtitle">
+                <span className="highlight">KREDITPE </span>
+                <span className="sub-highlight">INSTANT LOANS </span>
+                <span className="new-highlight">Within 10 Minutes</span>
+            </p>
+        </div>
+
+        <div className="sub-header">
+            Instant Loan | 100% Digital | Collateral-Free Loan | NO CREDIT HISTORY NEED
+        </div>
+
+        <hr className="divider" />
+
+        <div className="row">
+            <div className="box">
+                <div className="firstheading">
+                     <img src={images?.docimage} alt="docimage" className="img1" />
+                    <div className="headName">Documents Required</div>
+                </div>
+                <div className="list1">
+                    <ul>
+                        <li>Aadhaar Card</li>
+                        <li>PAN Card</li>
+                        <li>Bank Statement Certificate</li>
+                        <li>Photo</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="box box-tall">
+            <div className="secondheading">
+            <img src={images?.check} alt="check" className="img2" />
+                    <div className="headName2">Who can Apply here</div>
+                </div>
+                <div className="list2"></div>
+            </div>
+        </div>
+
+        {/* <div className="row row-spaced">
+            <div className="box1"></div>
+            <div className="box1 box-tall1"></div>
+        </div> */}
+    </div>
+</>
+    )
 };
 
 export default FormandInfo;
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,12 @@
 import prefr from "./images/prefr.svg";
 import logo from "./images/logo.png";
-
+import docimage from './images/image12.png'
+import check from './images/image15.png'
 const images = {
   prefr,
-  logo
+  logo,
+  docimage,
+  check
 };
 
 export default images;
