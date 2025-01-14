5cc625d2564ab13f99cd5bfe88df28b1a4568dbe
Vijaysharmagpj <vijaysharmagpj65@gmail.com>
2025-01-14 21:26:32 +0530

    new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Component/Navbar";
 import Dashboard from "./Component/Dashboard";
+import OurStory from "./Component/OurStory";
 
 const App=()=>{
   return(
     <>
     <Navbar/>
     <Dashboard/>
+    <OurStory/>
     </>
   )
 }
--- src/Component/OurStory.js
+++ src/Component/OurStory.js
@@ -1,13 +1,21 @@
 import React from "react";
+import img1 from "../assets/image.png";
+import "./OurStory.css";
 
-const OurStory=()=>{
-    return(
-        <>
-        <div>
-            <p>Our Story so far</p>
-            <p></p>
-        </div>
-        </>
-    )
-}
+const OurStory = () => {
+  return (
+    <>
+      <div className="story-title">
+        <p>Our Story so far</p>
+      </div>
+
+      <div className="story-container">
+        <img src={img1} alt="Our Story" className="story-image" />
+        <p className="story-number">1,50,000+</p>
+        <p className="family">Happy Family</p>
+      </div>
+    </>
+  );
+};
+
 export default OurStory;
