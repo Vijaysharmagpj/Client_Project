10a8ab65351765ea5b7ed00a62b61099d249281f
anujsingh2712 <singhanuj2507@gmail.com>
2025-01-15 00:34:08 +0530

    footer

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,35 @@
 import prefr from "./images/prefr.svg";
 import logo from "./images/logo.png";
+import QR from "./images/QR.png";
+import Link from "./images/Link.png"
+import playStore from "./images/play-store.png.png"
+import mail from "./images/mail.svg";
+import contact from "./images/Contact.svg";
+import adityaBirla from "./images/adityaBirlaGroup.png";
+import fimable from "./images/fimable.png"
+import hero from "./images/heroFinCorp.png";
+import poonawalla from "./images/poonawalla.png";
+import Razorpay from "./images/Razaorpay.png";
+import unity from "./images/unity.png";
+import zest from "./images/zest.png"
+import iifl from "./images/iifl.png"
 
 const images = {
+contact,
   prefr,
-  logo
+  logo,
+  QR,
+  Link,
+  playStore,
+  mail,
+  adityaBirla,
+  fimable,
+  hero,
+  Razorpay,
+  poonawalla,
+  unity,
+  zest,
+  iifl
 };
 
 export default images;
