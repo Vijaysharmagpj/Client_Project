02a66d70279e9f4d51550021fd7c928d4f9ef6ef
Vijaysharmagpj <vijaysharmagpj65@gmail.com>
2025-01-14 18:46:34 +0530

    add a new component

---
+++ src/Component/OurStory.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OurStory=()=>{
+    return(
+        <>
+        <div>
+            <p>Our Story so far</p>
+        </div>
+        </>
+    )
+}
+export default OurStory;
