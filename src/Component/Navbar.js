0f864b525e7efa0c472c93c51ff3340db2c44160
Vijaysharmagpj <vijaysharmagpj65@gmail.com>
2025-01-14 17:24:25 +0530

    Navbar Done

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -4,25 +4,20 @@ import logo from "../assets/logo.png";
 const Navbar = () => {
   return (
     <>
-      <div className="relative">
-        <div className="absolute top-[29px] left-[49px] flex items-center">
+      <div className="flex items-center justify-between px-8 py-4 bg-white">
+        <div className="flex items-center">
           <img src={logo} alt="logo" className="w-[61px] h-[57px]" />
-          <div className="ml-[20px]">
-            <p
-              className="font-[Geologica] font-bold text-[35px] leading-[43.75px] w-[175px] h-[44px]"
-              style={{ position: "absolute", top: "23px", left: "129px" }}
-            >
+          <div className="ml-4">
+            <p className="font-[Geologica] font-bold text-[35px] leading-[43.75px]">
               KREDITPE
             </p>
-            <p
-              className="font-[Gautami] font-bold text-[15px] leading-[28.59px] w-[131px] h-[29px]"
-              style={{ position: "absolute", top: "64px", left: "150px" }}
-            >
+            <p className="font-[Gautami] font-bold text-[15px] leading-[28.59px]">
               Loan in 10 min Only
             </p>
           </div>
         </div>
-        <div className="absolute top-[150px] left-[49px] flex gap-4">
+
+        <div className="flex gap-4">
           <button className="bg-black text-white px-4 py-2 rounded hover:bg-gray-800">
             APPLE
           </button>
