a9c467b5c63b2c354a5cf461eb4d9f7c53c3e6b3
anujsingh2712 <singhanuj2507@gmail.com>
2025-01-16 01:13:33 +0530

    "."

--- src/Component/Dashboard.jsx
+++ src/Component/Dashboard.jsx
@@ -5,34 +5,37 @@ import images from "../assets/index"
 
 const Dashboard = () => {
   return (
+    <>
     <div className="dashboard-container">
       <div className="dashboard-content">
         <div className="dashboard-header">
           <p className="header-title">
             APPLY INSTANT LOAN HERE FOR UPTO - 5 LACS NOW.
           </p>
-          <span className="header-subtitle">
+          <p> <span className="header-subtitle">
             IF YOU ARE A
           </span>
-          <span className="header-highlight">
-            (STOCK TRADERS/BAD CIBIL PROFILE/UN-EMPLOYED/SHOP KEEPERS/SMALL
-            BUSINESSMEN...ETC)
-          </span>
+            <span className="header-highlight">
+              (STOCK TRADERS/BAD CIBIL PROFILE/UN-EMPLOYED/SHOP KEEPERS/SMALL
+              BUSINESSMEN...ETC)
+            </span></p>
           <div className="loan-services">
-        <p className="services-description">
-          Our loan services offer a hassle-free and streamlined borrowing
-          experience, providing you with the funds you need in a timely manner
-          to meet your financial requirements.
-        </p>
-      </div>
+            <p className="services-description">
+              Our loan services offer a hassle-free and streamlined borrowing
+              experience, providing you with the funds you need in a timely manner
+              to meet your financial requirements.
+            </p>
+          </div>
         </div>
         <div className="image-dashboard">
-          <img src={images.bro} alt="images"/>
+          <img src={images.bro} alt="images" />
         </div>
       </div>
 
-      <button className="getstart">Get started</button>
     </div>
+      <button className="getstart">Get started</button>
+    
+    </>
   );
 };
 
