912535dc1c69f2870daf8acaf8e63a1e2a980332
anujsingh2712 <singhanuj2507@gmail.com>
2025-01-15 23:40:00 +0530

    minor changes

--- src/Component/Getstart.jsx
+++ src/Component/Getstart.jsx
@@ -10,7 +10,9 @@ const Getstart = () => {
           Get Started&nbsp;<span>quick and easy</span>
         </p>
       </div>
-      <div className="container">
+
+
+      {/* <div className="container">
         <div  className="image-container">
         <img className="bg-image" src={images?.bg} alt="bg" />
         <img className="kyc-image" src={images.CheckEligibility} alt="kyc" />
@@ -37,6 +39,41 @@ const Getstart = () => {
           <img className="bg-image" src={images?.bg} alt="bg" />
           <img className="kyc-image" src={images.readyforloan} alt="ready for loan" />
           <p>Ready For Loan</p>
+        </div>
+      </div> */}
+
+
+      <div className="_attritubes_container">
+        <div className="_attribute_container_col">
+          <div className="circular_bg">
+             <img src={images.readyforloan} alt="readyforloan"/>
+          </div>
+          <h2 className="attribute_container_col_heading">Check <br /> Eligibility</h2>
+        </div>
+        
+         <div className="_attribute_container_col">
+          <div className="circular_bg">
+             <img src={images.readyforloan} alt="readyforloan"/>
+          </div>
+          <h2 className="attribute_container_col_heading">Check <br /> Eligibility</h2>
+        </div>
+        <div className="_attribute_container_col">
+          <div className="circular_bg">
+             <img src={images.readyforloan} alt="readyforloan"/>
+          </div>
+          <h2  className="attribute_container_col_heading">Check <br /> Eligibility</h2>
+        </div>
+        <div className="_attribute_container_col">
+          <div className="circular_bg">
+             <img src={images.readyforloan} alt="readyforloan"/>
+          </div>
+          <h2  className="attribute_container_col_heading">Check <br /> Eligibility</h2>
+        </div>
+        <div className="_attribute_container_col">
+          <div className="circular_bg">
+             <img src={images.readyforloan} alt="readyforloan"/>
+          </div>
+          <h2  className="attribute_container_col_heading">Check  <br />Eligibility</h2>
         </div>
       </div>
     </>
